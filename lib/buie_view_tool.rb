a6a3d42c4c5350a4f4aa72f48b7774ae7ad299df
rebeccabuie <rebeccabuie@gmail.com>
2017-01-23 12:06:55 -0700

    Dev tool initail commit

---
+++ lib/buie_view_tool.rb
@@ -0,0 +1,6 @@
+require "buie_view_tool/version"
+require "buie_view_tool/renderer"
+
+module BuieViewTool
+  # Your code goes here...
+end
---
+++ lib/buie_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BuieViewTool
+  VERSION = "0.1.0"
+end
